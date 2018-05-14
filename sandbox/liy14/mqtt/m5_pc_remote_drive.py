b087248be074789774587e261e1034ca037e3a30
baix2 <baix2@rose-hulman.edu>
2018-05-14 10:47:55 -0400

    w

--- sandbox/liy14/mqtt/m5_pc_remote_drive.py
+++ sandbox/liy14/mqtt/m5_pc_remote_drive.py
@@ -63,8 +63,8 @@ def main():
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
     # forward_button and '<Up>' key is done for your here...
-    forward_button['command'] = lambda: some_callback1(mqtt_client, left_speed_entry, right_speed_entry)
-    root.bind('<Up>', lambda event: some_callback1(mqtt_client, left_speed_entry, right_speed_entry))
+    forward_button['command'] = lambda: forward(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<Up>', lambda event: forward(mqtt_client, left_speed_entry, right_speed_entry))
 
     left_button = ttk.Button(main_frame, text="Left")
     left_button.grid(row=3, column=0)
